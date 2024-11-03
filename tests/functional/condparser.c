14ef85fe4425df9a17bc6995a8e4777f10ea440c
Andrei Tsurkan <andrey.turcan@hotmail.com>
2024-11-03 22:37:27 +0000

    [condparser] test against compiler

--- tests/functional/condparser.c
+++ tests/functional/condparser.c
@@ -21,34 +21,36 @@ void condParserTestError(const char* msg)
     fprintf(stderr, "%s", msg);
 }
 
+#define COND_TEST(expr) { #expr, expr }
+
 UTEST(condparser, simple) {
     const CondParserTest tests[] = {
-        { "true", true },
-        { "false", false },
-        { "true && true", true },
-        { "true && false", false },
-        { "false || true", true },
-        { "false || false", false },
-        { "!true", false },
-        { "!false", true },
-        { "true || false && false", true },           // Checks precedence of && over ||
-        { "true && true || false", true },            // Checks precedence of && over ||
-        { "false || true && false", false },          // Checks mixed precedence
-        { "!(true && false)", true },                 // Checks negation with parentheses
-        { "!true || false", false },                  // Checks precedence of ! and ||
-        { "!(false || true) && true", false },        // Checks negation with mixed operators
-        { "true && (false || true)", true },          // Tests parentheses around ||
-        { "(true || false) && false", false },        // Tests parentheses around ||
-        { "!(true && true) || (false && true)", false }, // Combination with negation and parentheses
-        { "!(false || false) && (true || false)", true }, // Negation and mixed operators
-        { "(!true || true) && (true || !false)", true },  // Multiple negations and operators
-        { "true || !(false && true)", true },         // Negation within && condParserition
-        { "(true || false) && !(true && false)", true }, // Tests with &&, ||, and !
-        { "!(true && true) || false", false },        // Outer negation and || with false
-        { "!((true || false) && (true && true))", false }, // Complex nested structure
-        { "!!true", true },                           // Double negation
-        { "!((true || false) && !(false || true))", true },  // Nested negations and operators
-        { "(!((true && false) || (true || false) && !(false || !true)) && (true || false && true) || (!(true && (false || !false)) || !!false))", false } // Complex nested structure
+        COND_TEST(true),
+        COND_TEST(false),
+        COND_TEST(true && true),
+        COND_TEST(true && false),
+        COND_TEST(false || true),
+        COND_TEST(false || false),
+        COND_TEST(!true),
+        COND_TEST(!false),
+        COND_TEST(true || false && false),           // Checks precedence of && over ||
+        COND_TEST(true && true || false),            // Checks precedence of && over ||
+        COND_TEST(false || true && false),          // Checks mixed precedence
+        COND_TEST(!(true && false)),                 // Checks negation with parentheses
+        COND_TEST(!true || false),                  // Checks precedence of ! and ||
+        COND_TEST(!(false || true) && true),        // Checks negation with mixed operators
+        COND_TEST(true && (false || true)),          // Tests parentheses around ||
+        COND_TEST((true || false) && false),        // Tests parentheses around ||
+        COND_TEST(!(true && true) || (false && true)), // Combination with negation and parentheses
+        COND_TEST(!(false || false) && (true || false)), // Negation and mixed operators
+        COND_TEST((!true || true) && (true || !false)),  // Multiple negations and operators
+        COND_TEST(true || !(false && true)),         // Negation within && condParserition
+        COND_TEST((true || false) && !(true && false)), // Tests with &&, ||, and !
+        COND_TEST(!(true && true) || false),        // Outer negation and || with false
+        COND_TEST(!((true || false) && (true && true))), // Complex nested structure
+        COND_TEST(!!true),                           // Double negation
+        COND_TEST(!((true || false) && !(false || true))),  // Nested negations and operators
+        COND_TEST((!((true && false) || (true || false) && !(false || !true)) && (true || false && true) || (!(true && (false || !false)) || !!false))) // Complex nested structure
     };
 
     const int numTests = sizeof(tests) / sizeof(tests[0]);
